4533a7f491ae9e9f1250336d6dcdd018b3a0f441
Daniel <hernandezpdaniele@gmail.com>
2020-06-15 10:57:56 -0400

    update preloader.

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -5,10 +5,6 @@ $(window).on('load', () => {
     const preloader = $('.spinner-wrapper');
     setTimeout(function () {
         preloader.fadeOut(500);
-        $('header').fadeIn(1000);
-        $('main').fadeIn(1000);
-        $('footer').fadeIn(1000);
-        $('#curve').fadeIn(1000);
-        $('.corner-ribbon').fadeIn(1000);
+        $('content-wrapper').fadeIn(1000);
     }, 500);
 });
